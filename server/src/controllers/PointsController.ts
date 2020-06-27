8c2bc4694157b8cd5cdab93efaec537b1c35b023
HallanCosta <hallan.costa1@hotmail.com>
2020-06-27 01:53:19 -0300

    Uploads de imagens com multer completo

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -10,15 +10,22 @@ class PointsController {
       .split(',')
       .map(item => Number(item.trim()));
 
-      const points = await knex('points')
-        .join('point_items', 'points.id', '=', 'point_items.point_id')
-        .whereIn('point_items.item_id', parsedItems)
-        .where('city', String(city))
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
         .where('uf', String(uf))
-        .distinct()
-        .select('points.*');
+      .distinct()
+      .select('points.*');
 
-    return response.json(points);
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.1.105:3333/uploads/${point.image}`,
+      };
+    }); 
+
+    return response.json(serializedPoints);
   }
 
   async show(request: Request, response: Response) {
@@ -30,12 +37,17 @@ class PointsController {
       return response.status(400).json({ message: "Point not found." });
     }
 
+    const serializedPoint = {
+      ...point,
+      image_url: `http://192.168.1.105:3333/uploads/${point.image}`,
+    }; 
+
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title')
 
-    return response.json({ point, items });
+    return response.json({ point: serializedPoint, items });
   }
 
 
@@ -56,7 +68,7 @@ class PointsController {
     const trx = await knex.transaction(); 
 
     const point = {
-      image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -70,7 +82,10 @@ class PointsController {
 
     const point_id = insertedIds[0];
 
-    const pointItems = items.map((item_id: number) => {
+    const pointItems = items
+    .split(',')
+    .map((item: string) => Number(item.trim()))
+    .map((item_id: number) => {
       return {
         item_id,
         point_id: point_id,
